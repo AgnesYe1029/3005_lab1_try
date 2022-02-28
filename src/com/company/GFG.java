0c8fcd31089d73f4f0b1bd0a53bec2f7b8312a34
ziyuan Ye <ziyuanye@ziyuans-MacBook-Pro.local>
2022-03-01 01:36:43 +0800

    Initial commit - draft done for task1

---
+++ src/com/company/GFG.java
@@ -0,0 +1,234 @@
+package com.company;
+
+import java.util.*;
+
+/**Task 1
+ * You will need to solve a relaxed version of the NYC instance where we do not have the energy
+ * constraint. You can use any algorithm we discussed in the lectures. Note that this is equivalent to
+ * solving the shortest path problem.
+ *
+ */
+
+// Java Program to Implement Dijkstra's Algorithm
+// Using Priority Queue
+// Main class DPQ
+public class GFG {
+
+    // Member variables of this class
+    private Double dist[];
+    private static long energy_cost;
+    private static int[] prev; //previous path before reaching the destination
+    private Set<Integer> settled;
+    private PriorityQueue<Node> pq;
+    // Number of vertices
+    private int V;
+    List<List<Node>> adj;
+    // energy cost array
+    List<List<Node>> cost_adj;
+
+
+    // constructor of this class
+    public GFG(int V) {
+        this.V = V;
+        this.energy_cost = 0;
+        dist = new Double[V];
+        prev = new int[V];
+        settled = new HashSet<Integer>();
+        pq = new PriorityQueue<Node>(V, new Node());
+    }
+
+    // Method 1
+    // Dijkstra's Algorithm
+    public void dijkstra(List<List<Node>> adj, List<List<Node>> cost_adj, int src) {
+        this.adj = adj;
+        this.cost_adj = cost_adj;
+
+        for (int i = 0; i < V; i++) {
+            dist[i] = Double.MAX_VALUE;
+            prev[i] = -1; //initialize all the predecessors of all nodes to be -1
+        }
+
+        // Add source node to the priority queue
+        pq.add(new Node(src, 0.0));
+
+        // Distance to the source is 0
+        dist[src] = 0.0;
+
+        while (settled.size() != V) {
+
+            // Terminating condition check when
+            // the priority queue is empty, return
+            if (pq.isEmpty())
+                return;
+
+            // Removing the minimum distance node
+            // from the priority queue
+            int u = pq.remove().node;
+
+            // Adding the node whose distance is
+            // finalized
+            if (settled.contains(u))
+
+                // Continue keyword skips execution for
+                // following check
+                continue;
+
+            // We don't have to call e_Neighbors(u)
+            // if u is already present in the settled set.
+            settled.add(u);
+
+            e_Neighbours(u);
+        }
+    }
+
+    // Method 2
+    // To process all the neighbours
+    // of the passed node
+    private void e_Neighbours(int u) {
+
+        Double edgeDistance = -1.0;
+        Double newDistance = -1.0;
+
+        // All the neighbors of v
+        for (int i = 0; i < adj.get(u).size(); i++) {
+            Node v = adj.get(u).get(i);
+
+            // If current node hasn't already been processed
+            if (!settled.contains(v.node)) {
+                edgeDistance = v.cost;
+                newDistance = dist[u] + edgeDistance;
+
+                // If new distance is cheaper in cost
+                if (newDistance < dist[v.node]) {
+                    dist[v.node] = newDistance;
+                    prev[v.node] = u; //remember the predecessor
+                    for (int j = 0; j < cost_adj.get(u).size(); j++) {
+                        if (cost_adj.get(u).get(j).node == v.node) {
+                            double new_cost = cost_adj.get(u).get(j).cost;
+                            energy_cost += Math.round(new_cost);
+                            break;
+                        }
+                    }//adding up the cost
+
+                }
+
+                // Add the current node to the queue
+                pq.add(new Node(v.node, dist[v.node]));
+            }
+        }
+    }
+
+    //
+    public static void solveTask1(int numOfNodes, Map<int[], Double> distMap_int, Map<int[], Integer> costMap_int) {
+        int V = numOfNodes;
+        int source = 0;
+
+        // Adjacency list representation of the
+        // connected edges by declaring List class object
+        // Declaring object of type List<Node>
+        List<List<Node>> adj
+                = new ArrayList<List<Node>>();
+
+        // Initialize list for every node
+        for (int i = 0; i < V; i++) {
+            List<Node> item = new ArrayList<Node>();
+            adj.add(item);
+        }
+
+        // Inputs for the GFG(dpq) graph
+        // Iterating HashMap through for loop
+        for (Map.Entry<int[], Double> set :
+                distMap_int.entrySet()) {
+            int nodex = set.getKey()[0] - 1;//node need to be converted.
+            int nodey = set.getKey()[1] - 1;
+            Double dist = set.getValue();
+            adj.get(nodex).add(new Node(nodey, dist));
+            adj.get(nodey).add(new Node(nodex, dist));
+        }
+
+        // Adjacency list representation of the energy cost
+        List<List<Node>> cost_adj
+                = new ArrayList<List<Node>>();
+
+        // Initialize list for every node
+        for (int i = 0; i < V; i++) {
+            List<Node> item = new ArrayList<Node>();
+            cost_adj.add(item);
+        }
+
+        // Inputs for the cost energy adj list
+        for (Map.Entry<int[], Integer> set :
+                costMap_int.entrySet()) {
+            int nodea = set.getKey()[0] - 1;//node need to be converted.
+            int nodeb = set.getKey()[1] - 1;
+            double energy = set.getValue();
+            cost_adj.get(nodea).add(new Node(nodeb, energy));
+            cost_adj.get(nodeb).add(new Node(nodea, energy));
+        }
+
+        // Calculating the single source shortest path
+        GFG dpq = new GFG(V);
+        dpq.dijkstra(adj, cost_adj, source);
+
+        // Print shortest path
+        System.out.print("Shortest path: ");
+        int j = 49;
+        ArrayList<Integer> finalPath = new ArrayList<Integer>();
+        while (j >= 0) {
+            finalPath.add(0, j);
+            j = prev[j];
+        }
+        int val = 0;
+        while (finalPath.size() > val) {
+            System.out.print(finalPath.get(val) + 1);
+            if (val != finalPath.size() - 1)
+                System.out.print("->");
+            else System.out.print("\n");
+            val++;
+        }
+
+        //print shortest distance
+        System.out.println("Shortest distance: " + dpq.dist[49]);
+
+        //print energy cost
+        System.out.println("Total energy cost: " + energy_cost);
+
+    }
+}
+
+// Class 2
+// Helper class implementing Comparator interface
+// Representing a node in the graph
+class Node implements Comparator<Node> {
+
+    // Member variables of this class
+    public int node;
+    public Double cost;
+
+    // Constructors of this class
+
+    // Constructor 1
+    public Node() {}
+
+    // Constructor 2
+    public Node(int node, Double cost)
+    {
+
+        // This keyword refers to current instance itself
+        this.node = node;
+        this.cost = cost;
+    }
+
+    // Method 1
+    @Override public int compare(Node node1, Node node2)
+    {
+
+        if (node1.cost < node2.cost)
+            return -1;
+
+        if (node1.cost > node2.cost)
+            return 1;
+
+        return 0;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,118 @@
+package com.company;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.*;
+import java.net.URL;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        /**
+         * Preparation: Handling the input json files
+         *
+         */
+
+        //read Dist.json
+        URL distPath = Main.class.getResource("Dist.json");
+        File distFile = new File(distPath.getFile());
+        Reader distReader = new BufferedReader(new FileReader(distFile));
+        //Dist.json file is stored into a hashmap.
+        Map<String, Double> distMap = new Gson().fromJson(distReader,
+                new TypeToken<HashMap<String, Double>>() {}.getType());
+        //convert the distmap keys from string to intarray;
+        HashMap<int[], Double> distMap_intArray = new HashMap<int[], Double>();
+        for (Map.Entry<String, Double> set :
+                distMap.entrySet()) {
+
+            String temp = set.getKey().replaceAll("^\"|\"$", "");;
+            String[] strArray = temp.split(",");
+            int[] intArray = new int[strArray.length];
+            for(int i = 0; i < strArray.length; i++) {
+                intArray[i] = Integer.parseInt(strArray[i]);
+            }
+            distMap_intArray.put(intArray, set.getValue());
+        } //now that the distmap_intarray has int list keys. the keys follow [1, 2].
+
+        //read G.json
+        URL gPath = Main.class.getResource("G.json");
+        File gFile = new File(gPath.getFile());
+        Reader gReader = new BufferedReader(new FileReader(gFile));
+        //G.json file is stored into a hashmap of key type string and value type a list.
+        Map<Integer, List<String>> gMap = new Gson().fromJson(gReader,
+                new TypeToken<HashMap<Integer, List<String>>>() {}.getType());
+
+        //read Coord.json
+        URL coordPath = Main.class.getResource("Coord.json");
+        File coordFile = new File(coordPath.getFile());
+        Reader coordReader = new BufferedReader(new FileReader(coordFile));
+        //Coord.json file is stored into a hashmap of key type string and value type a list.
+        Map<Integer, List<Integer>> coordMap = new Gson().fromJson(coordReader,
+                new TypeToken<HashMap<Integer, List<Integer>>>() {}.getType());
+
+        //read Cost.json
+        URL costPath = Main.class.getResource("Cost.json");
+        File costFile = new File(costPath.getFile());
+        Reader costReader = new BufferedReader(new FileReader(costFile));
+        //Cost.json file is stored into a hashmap.
+        Map<String, Integer> costMap = new Gson().fromJson(costReader,
+                new TypeToken<HashMap<String, Integer>>() {}.getType());
+        //convert the costmap keys from string to intarray;
+        HashMap<int[], Integer> costMap_intArray = new HashMap<int[], Integer>();
+        for (Map.Entry<String, Integer> set :
+                costMap.entrySet()) {
+
+            String temp = set.getKey().replaceAll("^\"|\"$", "");;
+            String[] strArray = temp.split(",");
+            int[] intArray = new int[strArray.length];
+            for(int i = 0; i < strArray.length; i++) {
+                intArray[i] = Integer.parseInt(strArray[i]);
+            }
+            costMap_intArray.put(intArray, set.getValue());
+        } //now that the costmap_intarray has int list keys. the keys follow [1, 2].
+
+        //Example - make sure the inputs are correctly handled.
+//        System.out.println(distMap.get("1,2"));
+//        System.out.println(gMap.get(1));
+//        System.out.println(coordMap.get(1));
+//        System.out.println(coordMap.size());
+//        for (Map.Entry<int[], Double> set :
+//                costMap_intArray.entrySet()) {
+//            if(set.getKey()[0]==107502 || set.getKey()[1] == 107502)
+//                System.out.println(Arrays.toString(set.getKey()) + " = " + set.getValue());
+//        }
+//        int[] temparr = new int[2];
+//        temparr[0] = 107503;
+//        temparr[0] = 107502;
+//
+//        System.out.println(costMap_intArray.containsKey(temparr));
+
+//        for (Map.Entry<String, Double> set :
+//                costMap.entrySet()) {
+////            System.out.println("for debugging the costMap_local");
+////            System.out.println(set.getKey());
+//            if (set.getKey().equals("1273,1277")) {
+//                System.out.println("for debugging the costMap_local"); //1->1363->1358->1357->1356->1276->1273->1277->1269->1267->1268->1284->1283->1282->1255
+//                System.out.println(set.getKey() + "=" + set.getValue());
+//            }
+//        }
+
+
+        /**
+         * Task 1
+         */
+        int V = coordMap.size(); // total number of nodes
+        GFG.solveTask1(V, distMap_intArray, costMap_intArray);
+
+
+
+    }
+
+}
+
+
+
+
+
